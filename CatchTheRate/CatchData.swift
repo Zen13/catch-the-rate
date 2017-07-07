741c7c7d64c5610740dd057aa84b61251f058e12
Zen13 <zen-job@mail.ru>
2017-07-07 17:28:40 +0300

    Lesson_1

---
+++ CatchTheRate/CatchData.swift
@@ -0,0 +1,18 @@
+//
+//  CatchData.swift
+//  CatchTheRate
+//
+//  Created by Zen on 07.07.17.
+//  Copyright © 2017 Zen. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class CatchData: Object {
+    dynamic var currencies: String = ""
+    dynamic var sourceCurrency: String = ""
+    dynamic var format: String = ""
+    dynamic var rezult: String = ""
+    
+}
